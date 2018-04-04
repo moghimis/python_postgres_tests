0dea154b1925fc5774b4016a22b96755f04f541a
Saeed <moghimis@gmail.com>
2018-04-04 14:47:54 -0400

    Update psql.sh

--- psql.sh
+++ psql.sh
@@ -1,14 +1,14 @@
 source activate postgres
 psql --host=stampy.db.elephantsql.com --port=5432 --username=zjppuixw
 
-#password:  AQCCUe8DnEFRhzaUvu6WiqL3xa9BXAP0
+#password: ******
 
 #import os
 #import psycopg2
 #import urlparse
 
 #urlparse.uses_netloc.append("postgres")
-#url = urlparse.urlparse('postgres://zjppuixw:AQCCUe8DnEFRhzaUvu6WiqL3xa9BXAP0@stampy.db.elephantsql.com:5432/zjppuixw)
+#url = urlparse.urlparse('postgres://zjppuixw:*****@stampy.db.elephantsql.com:5432/zjppuixw)
 #conn = psycopg2.connect(database=url.path[1:],
 #  user=url.username,
 #  password=url.password,
